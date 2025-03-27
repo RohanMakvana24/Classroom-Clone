fbaf834beaf4324dab584d486a19c7e0636b8464
Rohan Makvana <makvanar360@gmail.com>
2025-03-27 22:19:11 +0530

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,4 +4,10 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: "0.0.0.0",               // Allows external access
+    port: 5173,                     // Port used by Vite
+    strictPort: true,               // Ensures Vite uses the specified port
+    allowedHosts: ['.onrender.com'], // ✅ Allow all Render subdomains
+  },
 })
